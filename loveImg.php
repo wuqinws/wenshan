f032f9e7609812bc09affe7be6c0836bd58281c7
Ki <3439780232@qq.com>
2024-06-15 03:23:29 +0000

    LikeGirl v5.1.1 优化图片懒加载
    Signed-off-by: Ki <3439780232@qq.com>

--- loveImg.php
+++ loveImg.php
@@ -21,42 +21,22 @@ $resImg = mysqli_query($connect, $loveImg);
             while ($list = mysqli_fetch_array($resImg)) {
                 ?>
                 <idv
-                    class="img_card col-lg-4 col-md-6 col-sm-12 col-sm-x-12 <?php if ($text['Animation'] == "1") { ?>animated slideInUp delay-03s<?php } ?>">
+                    class="img_card col-lg-4 col-md-6 col-sm-12 col-sm-x-12 <?php if ($text['Animation'] == "1") { ?>animated zoomIn delay-03s<?php } ?>">
                     <div class="love_img">
-                        <img class="<?php if ($text['Animation'] == "1") { ?>animated zoomIn delay-03s<?php } ?>"
-                            src="../Style/img/Loading2.gif" data-src="<?php echo $list['imgUrl'] ?>"
-                            alt="<?php echo $list['imgText'] ?>" data-description="<?php echo $list['imgDatd'] ?>">
+                        <img data-funlazy="<?php echo $list['imgUrl'] ?>" alt="<?php echo $list['imgText'] ?>"
+                            data-description="<?php echo $list['imgDatd'] ?>">
+
                         <div class="words">
                             <i>Date：<?php echo $list['imgDatd'] ?></i>
                             <span><?php echo $list['imgText'] ?></span>
                         </div>
+
                     </div>
                 </idv>
                 <?php
             }
             ?>
         </div>
-        <script>
-            let num = document.getElementsByTagName('img').length;
-            let img = document.getElementsByTagName('img')
-            let n = 0
-            lozyLoad()
-            window.onscroll = lozyLoad
-
-            function lozyLoad() {
-                let seeHeight = document.documentElement.clientHeight
-                let scrollHeight = document.documentElement.scrollTop || document.body.scrollTop
-                for (let i = n; i < num; i++) {
-                    if (img[i].offsetTop < seeHeight + scrollHeight) {
-                        if (img[i].getAttribute('src') == '../Style/img/Loading2.gif') {
-                            img[i].src = img[i].getAttribute('data-src')
-                        }
-                        n = i + 1
-                    }
-                }
-            }
-
-        </script>
     </div>
     <?php
     include_once 'footer.php';
